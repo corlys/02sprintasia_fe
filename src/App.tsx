ffe8f41f4a8d55752d7a597ef27810bab5015604
corlys <duscae047@gmail.com>
2024-08-21 10:19:59 +0700

    feat: flip subtask mutation

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,14 @@ function App() {
   const flipTask = async (id: number, currentState: string) => {
     await axiosClient.post("/tasks/update", {
       id,
-      currentState: currentState === "finished" ? "ongoing" : "finished",
+      status: currentState === "finished" ? "ongoing" : "finished",
+    });
+  };
+
+  const flipSubTask = async (id: number, currentState: boolean) => {
+    await axiosClient.post("/subtasks/update", {
+      id,
+      completed: currentState ? "false" : "true",
     });
   };
 
@@ -34,6 +41,16 @@ function App() {
   const flipTaskMutation = useMutation({
     mutationFn: ({ id, currentState }: { id: number; currentState: string }) =>
       flipTask(id, currentState),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["tasks"],
+      });
+    },
+  });
+
+  const flipSubTaskMutation = useMutation({
+    mutationFn: ({ id, currentState }: { id: number; currentState: boolean }) =>
+      flipSubTask(id, currentState),
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ["tasks"],
@@ -71,43 +88,62 @@ function App() {
         {data &&
           data.tasks.map((task) => (
             <div
-              className="border-2 border-sky-50 rounded-lg flex flex-row items-center justify-between p-8 gap-4"
+              className="border-2 border-sky-50 rounded-lg flex flex-col items-center justify-center p-8 gap-4"
               key={task.id}
             >
-              <div className="flex flex-col items-start justify-center">
-                <h2 className="text-xl font-bold">{task.title}</h2>
-                <p className="text-md font-semibold">{task.description}</p>
-              </div>
-              <button
-                onClick={() => {
-                  deleteTaskMutation.mutate({
-                    id: task.id,
-                  });
-                }}
-                className="px-4 py-2 border rounded-xl bg-red-500 hover:bg-red-400 text-white"
-              >
-                Delete
-              </button>
-              <button className="px-4 py-2 border rounded-xl bg-orange-500 hover:bg-orange-400 text-white">
-                Edit
-              </button>
-              {task.subTasks.length === 0 && (
+              <div className="flex flex-row items-center justify-between gap-4">
+                <div className="flex flex-col items-start justify-center">
+                  <h2 className="text-xl font-bold">{task.title}</h2>
+                  <p className="text-md font-semibold">{task.description}</p>
+                </div>
                 <button
                   onClick={() => {
-                    flipTaskMutation.mutate({
+                    deleteTaskMutation.mutate({
                       id: task.id,
-                      currentState:
-                        task.status === "ongoing" ? "finished" : "ongoing",
                     });
                   }}
-                  className="px-4 py-2 border rounded-xl bg-green-500 hover:bg-green-400 text-white"
+                  className="px-4 py-2 border rounded-xl bg-red-500 hover:bg-red-400 text-white"
                 >
-                  Finish
+                  Delete
                 </button>
-              )}
-              {task.subTasks.length > 0 && (
-                <div>{progressPercentage(task.subTasks)} %</div>
-              )}
+                <button className="px-4 py-2 border rounded-xl bg-orange-500 hover:bg-orange-400 text-white">
+                  Edit
+                </button>
+                {task.subTasks.length === 0 && (
+                  <button
+                    onClick={() => {
+                      flipTaskMutation.mutate({
+                        id: task.id,
+                        currentState:
+                          task.status === "ongoing" ? "finished" : "ongoing",
+                      });
+                    }}
+                    className="px-4 py-2 border rounded-xl bg-green-500 hover:bg-green-400 text-white"
+                  >
+                    Finish
+                  </button>
+                )}
+                {task.subTasks.length > 0 && (
+                  <div>{progressPercentage(task.subTasks)} %</div>
+                )}
+              </div>
+              {task.subTasks.length > 0 &&
+                task.subTasks.map((subTask) => (
+                  <div className="flex flex-row items-center justify-between w-full">
+                    <p className="text-md font-semibold">{subTask.title}</p>
+                    <button
+                      onClick={() => {
+                        flipSubTaskMutation.mutate({
+                          id: subTask.id,
+                          currentState: subTask.completed,
+                        });
+                      }}
+                      className={`rounded-lg text-white px-3 py-2 ${subTask.completed ? "bg-green-500 hover:bg-green-400" : "bg-red-500 hover:bg-red-400"}`}
+                    >
+                      {subTask.completed ? "Completed" : "Incomplete"}
+                    </button>
+                  </div>
+                ))}
             </div>
           ))}
       </div>
